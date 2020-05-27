8d781d85764ef271fa4ca95fd67a25fdc23aecbc
0ng <1427508598@qq.com>
2020-05-27 10:11:45 +0800

    题库页面

--- sch/luogu/views.py
+++ sch/luogu/views.py
@@ -29,8 +29,41 @@ def question(request):
 
 
 def hub(request):
-
-    return render(request, "hub.html", {"questions": [1,1,1,1,1,1,1,1,1,1,1,1,1,1]})
+    ls = [
+        {"status": 0,
+         "no": "P1000",
+         "title": "超级玛丽游戏",
+         "link": "https://www.luogu.com.cn/problem/P1000",
+         "tag": "傻逼题",
+         "difficulty": "入门",
+         "pass": 0.4
+         },
+        {"status": 0,
+         "no": "P1001",
+         "title": "A+B Problem",
+         "link": "https://www.luogu.com.cn/problem/P1001",
+         "tag": "傻逼题",
+         "difficulty": "入门",
+         "pass": 0.6
+         },
+        {"status": 0,
+         "no": "P1002",
+         "title": "过河卒",
+         "link": "https://www.luogu.com.cn/problem/P1002",
+         "tag": "NOIp普及组",
+         "difficulty": "普及-",
+         "pass": 0.3
+         },
+        {"status": 0,
+         "no": "P1003",
+         "title": "铺地毯",
+         "link": "https://www.luogu.com.cn/problem/P1003",
+         "tag": "NOIp提高组",
+         "difficulty": "普及-",
+         "pass": 0.4
+         },
+    ]
+    return render(request, "hub.html", {"questions": ls})
 
 
 def registerEnter(request):
